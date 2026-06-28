293c32e99c24e78e02be58fc277311e1ddc98cd6
sm67866 <sm67866@gmail.com>
2026-06-28 11:27:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Payments Learning Lab Loaded");
+
+//Search Box//
+
+const searchBox = document.querySelector(".search-box");
+const glossaryItems = document.querySelectorAll(".glossary-item");
+
+searchBox.addEventListener("input", function () {
+    const searchTerm = searchBox.value.toLowerCase();
+
+    glossaryItems.forEach(function (item) {
+        const itemText = item.innerText.toLowerCase();
+
+        if (itemText.includes(searchTerm)) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+});
